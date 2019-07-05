12747decc20ef6112ce22dac2301b82479f01a6a
Dat dep trai <trantandat130497@gmail.com>
2019-07-05 15:05:14 +0700

    build project

---
+++ build/precache-manifest.19be6977c9ad48badb3dcf0ec67a5f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "317f4c06d99c43abca66",
+    "url": "/static/js/main.c7a04317.chunk.js"
+  },
+  {
+    "revision": "afb2b53e4c97051ae88f",
+    "url": "/static/js/2.7b5ed599.chunk.js"
+  },
+  {
+    "revision": "317f4c06d99c43abca66",
+    "url": "/static/css/main.fb2d3b76.chunk.css"
+  },
+  {
+    "revision": "afb2b53e4c97051ae88f",
+    "url": "/static/css/2.61042ef8.chunk.css"
+  },
+  {
+    "revision": "cdb9b52dd94bd1c2fff913de4c1a7c3e",
+    "url": "/index.html"
+  }
+];
